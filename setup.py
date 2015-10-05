005d1708b4d40333b54b24701d1fb5475a2e6ae1
Kjeld Jensen <kj@kjen.dk>
2015-10-05 06:27:51 +0200

    Updated to support the new FroboMind abstraction of remote control and mission planner.

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=['mission/keyboard_mission_node.py'],)
+  scripts=[''],)
 setup(**d)
 
